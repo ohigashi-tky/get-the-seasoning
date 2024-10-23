09dee77996bf3774476b2d2e795c29edbdc4c540
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2024-10-23 20:06:20 +0900

    Initial Commit

---
+++ GetTheSeasoningTests/GetTheSeasoningTests.swift
@@ -0,0 +1,17 @@
+//
+//  GetTheSeasoningTests.swift
+//  GetTheSeasoningTests
+//
+//  Created by 大東拓也 on 2024/10/23.
+//
+
+import Testing
+@testable import GetTheSeasoning
+
+struct GetTheSeasoningTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
