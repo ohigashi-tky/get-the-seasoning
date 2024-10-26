4a9042784d3101a273a08d9981e8198525bda85a
ohigashi-tky <97085725+ohigashi-tky@users.noreply.github.com>
2024-10-27 02:28:01 +0900

    Implementation completed for now

--- GetTheSeasoning/GameScene.swift
+++ GetTheSeasoning/GameScene.swift
@@ -6,35 +6,87 @@
 //
 
 import SpriteKit
+import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var audioPlayer: AVAudioPlayer?
     
     var arm: SKSpriteNode!
     var soysauce: SKSpriteNode!
+    var messageImage: SKSpriteNode!
+    
     var timerLabel: SKLabelNode!
+    var bestTime: TimeInterval = Double.greatestFiniteMagnitude
+    var bestTimeLabel: SKLabelNode!
+    var currentMessageLabel: SKLabelNode?
+    var resetButton: SKLabelNode!
+    
     var gameTimer: Timer?
     var messageTimer: Timer?
     var elapsedTime: TimeInterval = 0
-    var currentMessageLabel: SKLabelNode?
+    
+    var isIndicatingFlag: Bool = false
     
     // スワイプ開始位置
     var swipeStartPosition: CGPoint?
     
+    struct PhysicsCategory {
+        static let arm: UInt32 = 0x1 << 0 // 腕のカテゴリ
+        static let soySauce: UInt32 = 0x1 << 1 // 醤油のカテゴリ
+    }
+    
     // 画面が呼び出された時
     override func didMove(to view: SKView) {
+        playBackgroundMusic()
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0) // 重力無し
+        physicsWorld.contactDelegate = self // 衝突判定を使用するために
+        setupResetButton()
         setupGame()
     }
     
+    func playBackgroundMusic() {
+        // 音声ファイルのパスを取得
+        guard let url = Bundle.main.url(forResource: "bgm", withExtension: "mp3") else {
+            print("BGM file not exist")
+            return
+        }
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+            audioPlayer?.numberOfLoops = -1 // -1:無限ループ
+            audioPlayer?.volume = 0.1
+            audioPlayer?.play()
+        } catch {
+            print("error Playing BGM: \(error.localizedDescription)")
+        }
+    }
+    
     func setupGame() {
-        // タイマー表示
+        // タイム計測
         timerLabel = SKLabelNode(text: "タイム: 0秒")
-        timerLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 130)
+        timerLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 170)
+        timerLabel.fontSize = 40
+        timerLabel.fontColor = SKColor.white
         addChild(timerLabel)
+        
+        // 最高タイム
+        bestTimeLabel = SKLabelNode(text: "最高タイム:")
+        bestTimeLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 250)
+        bestTimeLabel.fontSize = 40
+        bestTimeLabel.fontColor = SKColor.yellow
+        addChild(bestTimeLabel)
         
         // 腕を表示
         let armTexture = SKTexture(imageNamed: "arm")
         arm = SKSpriteNode(texture: armTexture, size: CGSize(width: 150, height: 300))
-        arm.position = CGPoint(x: 0, y: -100)
+        arm.position = CGPoint(x: 0, y: -400)
+        // 腕に物理ボディを追加
+        arm.physicsBody = SKPhysicsBody(texture: armTexture, size: arm.size)
+        arm.physicsBody?.isDynamic = true
+        arm.physicsBody?.categoryBitMask = PhysicsCategory.arm
+        arm.physicsBody?.contactTestBitMask = PhysicsCategory.soySauce
+        arm.physicsBody?.collisionBitMask = PhysicsCategory.soySauce
         addChild(arm)
         
         // 醤油を表示(初回)
@@ -43,13 +95,33 @@ class GameScene: SKScene {
         startMessageTimer()
     }
     
+    // リセットボタン
+    func setupResetButton() {
+        let background = SKSpriteNode(color: SKColor.gray, size: CGSize(width: 120, height: 50))
+        background.position = CGPoint(x: 200, y: frame.maxY - 75)
+        addChild(background)
+
+        resetButton = SKLabelNode(text: "リセット")
+        print(frame.minX)
+        resetButton.position = CGPoint(x: 200, y: frame.maxY - 85)
+        resetButton.fontSize = 30
+        resetButton.fontColor = SKColor.white
+        addChild(resetButton)
+    }
+    
+    // 醤油を生成
     func spawnSoysauce() {
-        // 醤油を新しい位置に生成
-//        let randomX = CGFloat.random(in: 0...frame.width)
-//        let randomY = CGFloat.random(in: 0...frame.height)
         let soysauceTexture = SKTexture(imageNamed: "soysauce")
         soysauce = SKSpriteNode(texture: soysauceTexture, size: CGSize(width: 170, height: 200))
-        soysauce.position = CGPoint(x: 0, y: 0)
+        soysauce.position = CGPoint(x: 0, y: 100)
+        
+        // 醤油に物理ボディを追加
+        soysauce.physicsBody = SKPhysicsBody(texture: soysauceTexture, size: soysauce.size)
+        soysauce.physicsBody?.isDynamic = true
+        soysauce.physicsBody?.categoryBitMask = PhysicsCategory.soySauce
+        soysauce.physicsBody?.contactTestBitMask = PhysicsCategory.arm  // 接触したときに腕に通知
+        soysauce.physicsBody?.collisionBitMask = PhysicsCategory.arm    // 腕と背色すると衝突として判定される
+        soysauce.name = "soysauce"
         addChild(soysauce)
     }
     
@@ -58,31 +130,41 @@ class GameScene: SKScene {
         currentMessageLabel?.removeFromParent()
         
         let messageLabel = SKLabelNode(text: message)
-        messageLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 250)
+        messageLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 350)
+        messageLabel.fontSize = 40
+        messageLabel.fontColor = SKColor.white
+        messageLabel.zPosition = 2
         addChild(messageLabel)
         // 現在のメッセージ
         currentMessageLabel = messageLabel
-
-        // 一定時間後にメッセージを消す
-        let wait = SKAction.wait(forDuration: 2.0)
-        let remove = SKAction.removeFromParent()
-        messageLabel.run(SKAction.sequence([wait, remove])) {
-            self.currentMessageLabel = nil
-        }
     }
     
     func startMessageTimer() {
-        let interval = Double.random(in: 5.0...7.0);
+        // 指示中でないなら指示を行う
+        guard !isIndicatingFlag else { return }
+        isIndicatingFlag = true;
+
+        let interval = Double.random(in: 6.0...8.0);
         messageTimer = Timer.scheduledTimer(withTimeInterval: interval, repeats: true) { [weak self] _ in
-            self?.showMessage("醤油取って！")
-            self?.startTimer()
+            guard let self = self else { return }
+            // メッセージの吹き出し
+            let messageImageTexture = SKTexture(imageNamed: "messageImage")
+            messageImage = SKSpriteNode(texture: messageImageTexture, size: CGSize(width: 450, height: 110))
+            messageImage.position = CGPoint(x: frame.midX, y: frame.maxY - 330)
+            if let messageImage = self.messageImage {
+                self.messageImage.zPosition = 1
+                self.addChild(messageImage)
+            }
+            self.showMessage("醤油取って！")
+            self.startTimer()
         }
     }
     
+    // タイム計測
     func startTimer() {
-        // 既存のゲームタイマーを無効化
+        print("タイム計測START!!")
         gameTimer?.invalidate()
-        
+
         elapsedTime = 0
         gameTimer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { [weak self] _ in
             self?.elapsedTime += 0.01
@@ -91,58 +173,133 @@ class GameScene: SKScene {
     }
     
     func stopTimer() {
-        gameTimer?.invalidate() // タイマーを無効化
-        gameTimer = nil // タイマーをnilにする
+        gameTimer?.invalidate()
+        gameTimer = nil
+    }
+    
+    // 接触を始めたときに呼び出される
+    func didBegin(_ contact: SKPhysicsContact) {
+        // 醤油を掴んでる間は処理を行わない
+        if let existSoysauce = arm.children.first(where: { $0.name == "soysauce" }) {
+            return
+        }
+
+        let bodyA = contact.bodyA
+        let bodyB = contact.bodyB
+        // 腕と醤油が接触したとき
+        let isContact = (bodyA.categoryBitMask == PhysicsCategory.arm && bodyB.categoryBitMask ==           PhysicsCategory.soySauce) || (bodyA.categoryBitMask == PhysicsCategory.soySauce && bodyB.categoryBitMask == PhysicsCategory.arm)
+        if (isContact) {
+            if let soyNode = bodyB.categoryBitMask == PhysicsCategory.soySauce ? bodyB.node : bodyA.node {
+                // 醤油を手に固定
+                soyNode.removeFromParent()
+                arm.addChild(soyNode)
+                soyNode.position = CGPoint(x: 0, y: 100)
+                soyNode.physicsBody?.isDynamic = false
+                soyNode.name = "soysauce"
+            }
+        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        // スワイプ開始位置
         swipeStartPosition = touch.location(in: self)
-
-//        let location = touch.location(in: self)
-//        // 醤油がタッチされたかをチェック
-//        if soysauce.contains(location) {
-//            soysauce.removeFromParent() // 醤油を取った（消す）
-//            gameTimer?.invalidate() // タイマーを停止
-//            showMessage("醤油を取った！")
-//            stopTimer()
-//            
-//            // 新しい醤油を表示
-//            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-//                self.spawnSoysauce()
-//            }
-//        }
+        
+        // リセットボタン
+        if resetButton.contains(location) {
+            resetGame() // リセット処理を呼び出す
+        }
+    }
+    
+    func resetGame() {
+        currentMessageLabel?.removeFromParent()
+        messageImage?.removeFromParent()
+        elapsedTime = 0
+        timerLabel.text = String(format: "タイム: %.2f秒", elapsedTime)
+        stopTimer()
+        bestTimeLabel.text = "最高タイム:"
+        arm.position = CGPoint(x: 0, y: -400)
+        resetArmPosition()
+        // 醤油の初期化
+        if let soyNode = arm.childNode(withName: "soysauce") {
+            isIndicatingFlag = false;
+            soyNode.removeFromParent()
+            self.spawnSoysauce()
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first, let startPosition = swipeStartPosition else { return }
         let currentPosition = touch.location(in: self)
-        
+
         // スワイプに合わせて腕を移動
-        let deltaX = currentPosition.x - startPosition.x
-        let deltaY = currentPosition.y - startPosition.y
-        let newPosition = CGPoint(x: arm.position.x + deltaX, y: arm.position.y + deltaY)
-        arm.position = newPosition
+        if arm.contains(currentPosition) {
+            let diffX = currentPosition.x - startPosition.x
+            let diffY = currentPosition.y - startPosition.y
+            let newPosition = CGPoint(x: arm.position.x + diffX, y: arm.position.y + diffY)
+            arm.position = newPosition
+        }
         
+        // 醤油を掴んだ状態で下部に到達したら取ったことにする
+        if let soyNode = arm.children.first(where: { $0.name == "soysauce" }) {
+            let soyNodeGlobalPosition = soyNode.convert(soyNode.position, to: self) // グローバル座標に変換
+            if soyNodeGlobalPosition.y < frame.minY + (frame.height / 3.3) {
+                removeSoysauce()
+            }
+        }
+    
         // スワイプ開始位置を更新
         swipeStartPosition = currentPosition
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        // スワイプの終点で醤油に触れたかをチェック
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        
-        if soysauce.contains(location) {
-            soysauce.removeFromParent()
-            gameTimer?.invalidate()
-            showMessage("醤油を取った！")
-            stopTimer()
-            
-            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                self.spawnSoysauce()
-            }
+    func removeSoysauce() {
+        if let soyNode = arm.childNode(withName: "soysauce") {
+            soyNode.removeFromParent()
         }
+
+        // メッセージ吹き出しを削除
+        messageImage?.removeFromParent()
+        if elapsedTime < 0.5 {
+            showMessage("早すぎぃ！")
+        } else if elapsedTime < 1 {
+            showMessage("早い！")
+        } else if elapsedTime > 3 {
+            showMessage("遅い！")
+        } else {
+            showMessage("ありがとう！")
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.currentMessageLabel?.removeFromParent()
+            self.currentMessageLabel = nil
+        }
+        
+        // 最高タイムの更新
+        if elapsedTime < bestTime {
+            bestTime = elapsedTime
+            bestTimeLabel.text = String(format: "最高タイム: %.2f秒", bestTime)
+        }
+        
+        // 指示中フラグの解除
+        isIndicatingFlag = false;
+        // 腕を初期位置に戻す
+        resetArmPosition()
+        // タイマーを初期化
+        elapsedTime = 0
+        timerLabel.text = String(format: "タイム: %.2f秒", elapsedTime)
+        stopTimer()
+
+        // 2秒後に醤油を再生成
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.spawnSoysauce()
+        }
+    }
+    
+    func resetArmPosition() {
+        // 腕を初期位置に戻す
+        arm.position = CGPoint(x: 0, y: -400)
+        arm.zRotation = 0
     }
     
     override func willMove(from view: SKView) {
